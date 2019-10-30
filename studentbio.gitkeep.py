9fc950bf6328e7f4dccb8c809e4c72c0a19611db
liscruz <53804262+liscruz@users.noreply.github.com>
2019-10-30 19:43:00 -0400

    Add files via upload

---
+++ studentbio.gitkeep.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <title> Lisette Cruz</title>
+</head>
+
+<body>
+
+<img src= "‪C:\Users\liset\Desktop\princess jasmine.jpg" alt="Lisette Cruz">
+
+<p> Student Bio</p>
+
+<ul>
+    <li><strong>Email:</strong> <a href= liscruz071@gmail.com</a></li>
+    <li><strong>GitHub</strong> <a href= liscruz071@github.com</a></li>
+
+<table>
+    <Tr>
+        <th>Books</th>
+        <th>Movies</th>
+        <th>Games</th>
+    </tr>
+    <tr>
+        <td> The Alchemist
+        <td> Daily Devotional
+
